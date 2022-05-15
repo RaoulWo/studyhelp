1d923e2c0bb9d39fbd5d9ec16fc9d7894f6cef99
Raoul Wograndl <Wograndl.Raoul@outlook.com>
2022-05-16 00:03:50 +0200

    Dumps für die Datenbanken upgedatet

--- database/user_dump.sql
+++ database/user_dump.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
+-- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: user
 -- ------------------------------------------------------
--- Server version	10.4.24-MariaDB
+-- Server version	10.4.22-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,33 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `gameresults`
+--
+
+DROP TABLE IF EXISTS `gameresults`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gameresults` (
+  `game_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fk_user_id` int(11) NOT NULL,
+  `punkte` int(11) NOT NULL,
+  PRIMARY KEY (`game_id`),
+  KEY `fk_user_id` (`fk_user_id`),
+  CONSTRAINT `gameresults_ibfk_1` FOREIGN KEY (`fk_user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gameresults`
+--
+
+LOCK TABLES `gameresults` WRITE;
+/*!40000 ALTER TABLE `gameresults` DISABLE KEYS */;
+INSERT INTO `gameresults` VALUES (7,2,1300),(8,2,200),(9,2,300),(10,2,200),(11,2,100),(12,2,200),(13,2,300),(14,2,200),(15,2,100);
+/*!40000 ALTER TABLE `gameresults` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -27,8 +54,10 @@ CREATE TABLE `user` (
   `username` varchar(100) COLLATE utf8_german2_ci NOT NULL,
   `email` varchar(100) COLLATE utf8_german2_ci NOT NULL,
   `password` varchar(100) COLLATE utf8_german2_ci NOT NULL,
+  `level` int(11) NOT NULL DEFAULT 0,
+  `punkte` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,6 +66,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Raoul','Raoul.Wograndl@email.at','81dc9bdb52d04dc20036dbd8313ed055 ',0,0),(2,'Paul','Paul.Paul@Paul','81dc9bdb52d04dc20036dbd8313ed055',3,850);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-03 22:38:12
+-- Dump completed on 2022-05-16  0:02:09
